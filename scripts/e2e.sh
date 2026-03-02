8534c6a98364636816841d911b7a5c638a10e410
Jack Kingsman <jack.kingsman@gmail.com>
2026-03-02 10:46:48 -0800

    Add clearer message for e2e test lags

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -3,6 +3,6 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-echo "Starting E2E tests..."
+echo "Starting E2E tests (kicks off a build; this may take a few minutes..."
 cd "$SCRIPT_DIR/tests/e2e"
 npx playwright test "$@"
